88c54c9414c557b217acb6dc149efe2b2bae1133
Gibby Eidem <gibbyeidem@Gibbys-MacBook-Pro.local>
2023-05-05 17:50:11 -0600

    Refactored server.js to make sure functionality was correct

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs');
+const notes = require('./db/db.json');
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get( '/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
@@ -14,10 +17,35 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
+app.get('/api/notes', (req, res) => {
+    res.status(200).json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text
+        };
+
+        notes.push(newNote);
+
+        fs.writeFileSync(
+            path.join(__dirname, './db/db.json'),
+            JSON.stringify(notes)
+        );
+
+        res.status(200).json(newNote);
+    } else {
+        res.status(400).json('Error in posting note');
+    }
+});
 
 
 
 
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`Listening at http://localhost:${PORT}`)
 );
