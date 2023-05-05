9f3516de5ab11f7394ae611cda7c567527b5ab6d
Gibby Eidem <gibbyeidem@Gibbys-MacBook-Pro.local>
2023-05-05 16:18:38 -0600

    added another api call for /notes.html

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+
+
 
 
 app.listen(PORT, () =>
